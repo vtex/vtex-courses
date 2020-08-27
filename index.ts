4dbbb87cf853f09f9c7eddaa81208989d9b85256
Julia Rocha <juliaafr@outlook.com>
2020-08-27 12:39:35 -0300

    fix methods

--- index.ts
+++ index.ts
@@ -1,18 +1,16 @@
-// import { handleSteps } from './src/script/steps'
-// import { handleCourses } from './src/script/courses'
-// import { handleLanding } from './src/script/landing'
+import { handleSteps } from './src/script/steps'
+import { handleCourses } from './src/script/courses'
+import { handleLanding } from './src/script/landing'
 import { handleCoursesOrder } from './src/script/course-order'
 import configs from './src/utils/configs'
 
 const courses = configs()
 
-// slug -> courses[0].summary[0].folder
-
 const execute = async () => {
   await handleCoursesOrder(courses)
-  // await handleLanding(courses)
-  // await handleCourses(courses)
-  // await handleSteps(courses)
+  await handleLanding(courses)
+  await handleCourses(courses)
+  await handleSteps(courses)
 }
 
 execute()
--- src/script/course-order.ts
+++ src/script/course-order.ts
@@ -1,20 +1,19 @@
 import { Course } from '../../typings/course'
 import ReadMeServices from '../clients/readme-service'
 
-export const handleCoursesOrder = (courses: Course[]) =>
-Promise.all(
+export const handleCoursesOrder = (courses: Course[]) => {
+  const ReadMe = new ReadMeServices()
+  return Promise.all(
     courses.map(async (course) => {
       console.log(`Getting steps order from ${course.name}...`)
       const courseOrder = {
         course: course.name,
-        steps: course.summary.map((step => step.folder))
+        steps: course.summary.map((step) => step.folder),
       }
-    console.log(courseOrder)
-    await ReadMeServices.postStepsOrder(courseOrder)
 
-    await ReadMeServices.getStepsOrder('store-block')
+      await ReadMe.postStepsOrder(courseOrder)
 
-
-     console.log(`...updated steps order of ${course.name}`)
+      console.log(`...updated steps order of ${course.name}`)
     })
   )
+}
