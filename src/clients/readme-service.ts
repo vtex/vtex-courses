ffa9a517815cb73bf89b402f762086f99e31205d
Julia Rocha <juliaafr@outlook.com>
2020-08-27 12:39:35 -0300

    fix methods

--- src/clients/readme-service.ts
+++ src/clients/readme-service.ts
@@ -1,27 +1,17 @@
 import { CourseOrder } from '../../typings/course'
 import DefaultClient from './default'
-import fetch from 'node-fetch'
 
 export default class ReadMeService extends DefaultClient {
     constructor() {
-    super('https://juzao--vtex.myvtex.com/_v/readme/*', {})
+        super('https://juzao--vtex.myvtex.com/_v/readme/')
     }
 
-    public static async postStepsOrder(course: CourseOrder) {
-        await fetch('https://juzao--vtex.myvtex.com/_v/readme/saveOrder', {
-            method: 'POST',
-            body: JSON.stringify(course)
-        }).then(() => console.log('posted'))
-
-        return
+    public postStepsOrder(course: CourseOrder) {
+        return this.post('/order', course)
     }
 
-    public static async getStepsOrder(courseName: string) {
-        await fetch('https://juzao--vtex.myvtex.com/_v/readme/getOrder', {
-            method: 'POST',
-            body: JSON.stringify({course: courseName})
-        }).then(async stepsOrder => console.log(stepsOrder))
-        return
+    public getStepsOrder(courseName: string) {
+        return this.get(`/order/${courseName}`)
     }
 }
 
