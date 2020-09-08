0bc4a1f3f511917ba52bc9bfc5aba8bec61cad2f
Julia Rocha <juliaafr@outlook.com>
2020-09-08 14:50:18 -0300

    add finish course button and last step util

--- src/templates/step.ts
+++ src/templates/step.ts
@@ -1,5 +1,6 @@
 import { getStepStyles } from './stepStyles'
 import { BASE_PATH } from '../utils/constants'
+import { isLastStep } from '../utils/lastStep'
 
 export default (content: string, slug: string, hasAnswersheet: boolean) => `
   ${getStepStyles()}
@@ -11,5 +12,16 @@ export default (content: string, slug: string, hasAnswersheet: boolean) => `
 
   Confira [aqui o gabarito para esta etapa](${BASE_PATH}/docs/${slug}-answersheet) ou peça ajuda a um de nossos monitores`
       : ''
+  }
+
+  ${
+    isLastStep(slug)
+      ?
+      `<div
+        id="finish-course">
+        <i class="fa fa-chevron-right" ></i>
+        <a id="finish-link" href="/learning/page/training-week-learning-path">Finalizar Curso</a>
+      </div>`
+      : ``
   }
 `
