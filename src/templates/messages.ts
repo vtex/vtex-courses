94e0d38c7acbf545b3981990629ed52ee81739e5
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2023-01-31 11:06:59 -0300

    Update src/templates/messages.ts

--- src/templates/messages.ts
+++ src/templates/messages.ts
@@ -41,8 +41,8 @@ const messages: Record<string, LanguageSwitcher> = {
     en: 'See the [answersheet for this step]',
   },
   officeHours: {
-    pt: 'acompanhe no canal VTEX Developers nosso [office hours]',
-    en: 'check on the VTEX Developers channel our [office hours]',
+    pt: 'acompanhe nosso [office hours] no canal VTEX Developers',
+    en: 'check our [office hours] on the VTEX Developers channel',
   },
   answersheetTo: {
     pt: 'Gabarito para o passo',
