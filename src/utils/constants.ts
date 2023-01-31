923a70bfa68755746d80a4dfe8d1ffe6229b4662
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2023-01-31 11:57:44 -0300

    Update src/utils/constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,3 @@ export const REPO_URL = 'https://github.com/vtex/vtex-courses'
 export const REPO_EDIT_URL = (path: string) => `${REPO_URL}/edit/master/${path}`
 export const REPO_NEW_ISSUE_URL = `${REPO_URL}/issues/new/choose`
 export const OFFICE_HOURS_LINK = 'https://www.youtube.com/c/VTEXDevelopers'
-  'https://www.youtube.com/c/VTEXDevelopers'
